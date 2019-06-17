5b316d0ef9231b8a6eed46f46e9728827f39fee7
ZornitsaAtanassova <zornitsa.atanassova@gmail.com>
2019-06-17 17:25:26 +0300

    Met the slice, splice, map and forEach array methods

--- 17.LAB_Lists-ArraysAdvanced-12.06/02.negativePositiveNumbers.js
+++ 17.LAB_Lists-ArraysAdvanced-12.06/02.negativePositiveNumbers.js
@@ -1,7 +1,8 @@
 function sortArray(arr) {
     // Write a function that processes the elements in an array one by one and produces a new array. Prepend each negative element at the front of the result and append each positive (or 0) element at the end of the result. The input comes as array of number elements. The output is printed on the console, each element on a new line.
 
-    let newArr = [];
+    // 1 solve - with for loop + if-else conditional statement
+    /* let newArr = [];
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] < 0) {
             newArr.unshift(arr[i]);
@@ -12,7 +13,21 @@ function sortArray(arr) {
         // ternary operator:
         /* (arr[i] < 0)
             ? newArr.unshift(arr[i])
-            : newArr.push(arr[i]); */
+            : newArr.push(arr[i]); 
+    } */
+
+    // 2 solve - with for-of loop + ternary operator for conditional statement
+    let newArr = [];
+    for (let num of arr) {
+        (num < 0)
+        ? newArr.unshift(num)
+        : newArr.push(num);
+
+        /* if (num < 0) {
+            newArr.unshift(num);
+        } else {
+            newArr.push(num);
+        } */  
     }
 
     return console.log(newArr.join('\n'));
--- 17.LAB_Lists-ArraysAdvanced-12.06/Lab.js
+++ 17.LAB_Lists-ArraysAdvanced-12.06/Lab.js
@@ -1,37 +1,63 @@
 const numbers = ['1', '2', '3', '4', '5', '6'];
-console.log(numbers.join(', '));
+console.log(`=*=*=*=*=*=*join, push, pop, unshift, shift=*=*=*=*=*=*`);
+// method ".join('')". Return string from all array's elements
+console.log(numbers.join(' mississippi, '), numbers);
 
-// add at the tail end - element(cell and value)
-numbers.push('seven(7)');
-console.log(numbers.join(', '));
+// method ".push()". Add at the tail end - element(cell and value)
+const pushReturn = numbers.push('seven(7)');
+console.log(pushReturn, numbers);
 
-// remove from the tail end - element(cell and value)
-numbers.pop();
-console.log(numbers.join(', '));
+// method ".pop()". Remove from the tail end - element(cell and value)
+const popReturn = numbers.pop();
+console.log(popReturn, numbers);
 
-// add at the start - element(cell and value)
-numbers.unshift('zero(0)');
-console.log(numbers.join(', '));
+// method ".unshift()". Add at the start - element(cell and value)
+const unshiftReturn = numbers.unshift('zero(0)');
+console.log(unshiftReturn, numbers);
 
-// remove from the start - element(cell and value)
-numbers.shift();
-console.log(numbers.join(', '));
+//  method ".shift()". Remove from the start - element(cell and value)
+const shiftReturn = numbers.shift();
+console.log(shiftReturn, numbers);
+console.log(`=*=*=*=*=*=*join, push, pop, unshift, shift=*=*=*=*=*=*\n`);
 
-numbers.map(Number);
-console.log(numbers.join(', '));
+console.log(`**************slice**************`);
+//  method ".slice()". Copies range from the origin array to new array. First argument is START index/value (include), Second argument is END index/value (exclude)!
+const sliceReturn1 = numbers.slice('1', '3');
+const sliceReturn2 = numbers.slice(0);
+console.log(sliceReturn1, sliceReturn2, numbers);
+console.log(`**************slice**************\n`);
 
-// add at the middle - element(cell and value)
+console.log(`-----------sPlice-------------`);
+//  method ".splice()". Change origin array! Replace range from the origin array to new array. First argument is START index/value (include), Second argument is END index/value (exclude)!
+const spliceReturn1 = numbers.splice(1, 2);
+const spliceReturn2 = numbers.splice(1, 0, '2', '3');
+console.log(spliceReturn1, spliceReturn2, numbers);
+console.log(`-----------sPlice--------------\n`);
+
+//  method ".map()".
+const mapReturn = numbers.map(Number);
+console.log(mapReturn, numbers);
+
+// method ".forEach()". 
+let newVariableForEach = '';
+numbers.forEach((element) => {
+    newVariableForEach += (Number(element) + 100) + ' ';
+});
+console.log(newVariableForEach);
+console.log(numbers);
+
+//  operator "delete". Remove from anywhere - only value, cell stay and there is undefined. Not good for arrays! Use pop() and shift() instead!
+delete numbers[1];
+console.log(numbers);
+
+// algorithm for add at the middle - element(cell and value)
 /* for (let i = 1; i < numbers.length; i++) {
     let worker = [];
 
 } */
-
-// remove from anywhere - only value, cell stay and there is undefined. Not good for arrays!
-delete numbers[1];
-console.log(numbers.join(', '));
-
-
 console.log(`------------------------`);
+
+// Sorting array
 const arrNumbers = ['11', '15', '-123', '99', '5', '-5'];
 function sortNums(strArr) {
     const newSortedArr = [];
@@ -53,3 +79,24 @@ function sortNums(strArr) {
 }
 const newSortedArr = sortNums(arrNumbers);
 console.log(newSortedArr);
+console.log(arrNumbers);
+console.log(`------------------------`);
+
+// method ".forEach()"
+arrOne = [1, 2, 3];
+for (let i = 0; i <arrOne.length; i++) {
+    arrOne[i] += 10;
+}
+console.log(arrOne);
+
+let newArrOne = [];
+arrOne.forEach((element) => {
+    // element -= 10;
+    newArrOne.push(element -= 10);
+    // console.log(element);
+    arrOne.pop();
+});
+console.log(newArrOne);
+console.log(arrOne);
+console.log(`------------------------`);
+
