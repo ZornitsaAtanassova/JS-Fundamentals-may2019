8156355dbdb0b2a9f2ca06afed8c674455e06759
ZornitsaAtanassova <zornitsa.atanassova@gmail.com>
2019-06-18 12:28:41 +0300

    Met the filter() array method

--- 17.LAB_ArraysAdvanced-12.06/Lab.js
+++ 17.LAB_ArraysAdvanced-12.06/Lab.js
@@ -22,41 +22,48 @@ console.log(`=*=*=*=*=*=*join, push, pop, unshift, shift=*=*=*=*=*=*\n`);
 
 console.log(`**************slice**************`);
 //  method ".slice()". Copies range from the origin array to new array. First argument is START index/value (include), Second argument is END index/value (exclude)!
-const sliceReturn1 = numbers.slice('1', '3');
-const sliceReturn2 = numbers.slice(0);
-console.log(sliceReturn1, sliceReturn2, numbers);
+const sliceNewArrReturn1 = numbers.slice('1', '3');
+const sliceNewArrReturn2 = numbers.slice(0);
+console.log(sliceNewArrReturn1, sliceNewArrReturn2, numbers);
 console.log(`**************slice**************\n`);
 
 console.log(`-----------sPlice-------------`);
 //  method ".splice()". Change origin array! Replace range from the origin array to new array. First argument is START index/value (include), Second argument is END index/value (exclude)!
-const spliceReturn1 = numbers.splice(1, 2);
-const spliceReturn2 = numbers.splice(1, 0, '2', '3');
-console.log(spliceReturn1, spliceReturn2, numbers);
-console.log(`-----------sPlice--------------\n`);
-
-//  method ".map()".
-const mapReturn = numbers.map(Number);
-console.log(mapReturn, numbers);
-
-// method ".forEach()". 
-let newVariableForEach = '';
-numbers.forEach((element) => {
-    newVariableForEach += (Number(element) + 100) + ' ';
-});
-console.log(newVariableForEach);
-console.log(numbers);
-
-//  operator "delete". Remove from anywhere - only value, cell stay and there is undefined. Not good for arrays! Use pop() and shift() instead!
-delete numbers[1];
-console.log(numbers);
+const spliceNewArrReturn1 = numbers.splice(1, 2);
+const spliceNewArrReturn2 = numbers.splice(1, 0, '2', '3');
+console.log(spliceNewArrReturn1, spliceNewArrReturn2, numbers);
 
 // algorithm for add at the middle - element(cell and value)
 /* for (let i = 1; i < numbers.length; i++) {
     let worker = [];
 
 } */
-console.log(`------------------------`);
+console.log(`-----------sPlice--------------\n`);
 
+console.log(`==========map, forEach, filter==========`);
+//  method ".map()"
+const mapNewArrReturn = numbers.map(Number);
+console.log(mapNewArrReturn, numbers);
+
+// method ".forEach()"
+let forEachNewVariableReturn = '';
+numbers.forEach((element) => {
+    forEachNewVariableReturn += (Number(element) + 100) + ' ';
+});
+console.log(forEachNewVariableReturn, numbers);
+
+// method ".filter()"
+const filterNewNewArrReturn = numbers.filter((el) => {
+    return el % 2 === 0;
+});
+console.log(filterNewNewArrReturn, numbers);
+console.log(`==========map, forEach, filter==========\n`);
+
+//  operator "delete". Remove from anywhere - only value, cell stay and there is undefined. Not good for arrays! Use pop() and shift() instead!
+delete numbers[1];
+console.log(numbers);
+
+console.log(`------------------------`);
 // Sorting array
 const arrNumbers = ['11', '15', '-123', '99', '5', '-5'];
 function sortNums(strArr) {
