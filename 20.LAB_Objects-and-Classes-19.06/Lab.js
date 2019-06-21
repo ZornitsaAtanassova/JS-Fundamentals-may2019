a4658220f167217e9a871c73788fb17e6bf11c1f
ZornitsaAtanassova <zornitsa.atanassova@gmail.com>
2019-06-21 15:50:45 +0300

    Small fixes

--- 20.LAB_Objects-and-Classes-19.06/Lab.js
+++ 20.LAB_Objects-and-Classes-19.06/Lab.js
@@ -43,7 +43,7 @@
     const fewProperties = ['color', 'shape'];
     for (const prop in cup) {
         if (fewProperties.indexOf(prop) !== -1) {
-            console.log(prop, cup(prop));
+            // console.log(prop, cup(prop));
         }
     }
 // Добавяне или промяна на свойство
@@ -56,3 +56,25 @@
 
     console.log(cup);
 // Кое свойство от ООП (абстракция, наследяване, капсулиране) се ползва в JS?
+
+    let arr = [1, 2, 3];
+    arr.forEach(function(item, index, array) {
+        console.log(item, index, array);
+    });
+
+    let months = ['z', 'a', 'f', 'd'];
+    months.sort((a, b) => {
+        if (a < b) {
+            return true;
+        }
+    });
+    console.log(months);
+    console.log(months.indexOf(33));
+    console.log(months.includes(3));
+    console.log(months.join('*'));
+    console.log(months.toString());
+    console.log(months.map(function(x) { return x.toUpperCase(); }));
+    console.log(months.filter(char => char === 'a'));
+    months.forEach(function(item, index) {
+        console.log(item, index);
+    });
