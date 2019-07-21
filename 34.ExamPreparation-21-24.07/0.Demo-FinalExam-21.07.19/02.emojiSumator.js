6e5254693b17e33341c576a37bd797eafeb74753
Zornitsa Atanassova <zornitsa.atanassova@gmail.com>
2019-07-21 15:18:25 +0300

    Demo Final Exam from 21.07.19

---
+++ 34.ExamPreparation-21-24.07/0.Demo-FinalExam-21.07.19/02.emojiSumator.js
@@ -0,0 +1,13 @@
+function emojiSumator(input) {
+
+}
+
+emojiSumator([ 
+    'Hello I am Mark from :England: and I am :one: :seven: years old, I have a :hamster: as pet.',
+    '115:101:118:101:110' 
+]);     // Emojis found: :seven:, :hamster:
+// Total Emoji Power: 2602
+emojiSumator([ 
+    'In the Sofia Zoo there are many animals, such as :ti ger:, :elephan:t, :monk3y:, :goriLLa:, :fox:.',
+    '97:101:117:114' 
+]);     // Total Emoji Power: 0
